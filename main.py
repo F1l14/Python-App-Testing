7f5ce18343dd769706fa6a9c8c3680382d58450c
F1l14 <filipmn42@gmail.com>
2025-02-06 17:20:55 +0200

    window resizing app

--- main.py
+++ main.py
@@ -1,31 +1,45 @@
 from  PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout, QHBoxLayout
 
+window_size = 400
+
+def resize_window(option, label):
+    global window_size
+    if(option == "bigger"):
+        window_size += 100
+       
+    if(option == "smaller"):
+        window_size -= 100
+    main_win.resize(window_size, window_size)
+    label.setText(f'Window size: {main_win.size().width()} x {main_win.size().height()}')
+
 #! App Settings ----
 app = QApplication([])
 main_win = QWidget()
 main_win.setWindowTitle('Test App')
-main_win.resize(400, 400)
+main_win.resize(window_size, window_size)
 
 
 #! Objects ----
-button1 = QPushButton('Button 1')
-button2 = QPushButton('Button 2')
-button3 = QPushButton('Button 3')
 
+title = QLabel('Window size: 400 x 400')
+plusButton = QPushButton('+')
+plusButton.clicked.connect(lambda:resize_window("bigger", title))
+minusButton = QPushButton('-')
+minusButton.clicked.connect(lambda:resize_window("smaller", title))
 #! Design ----
 master_layout = QVBoxLayout()
 row1 = QHBoxLayout()
 row2 = QHBoxLayout()
-row3 = QHBoxLayout()
 
-row1.addWidget(button1, alignment=Qt.AlignLeft)
-row2.addWidget(button2, alignment=Qt.AlignCenter)
-row3.addWidget(button3, alignment=Qt.AlignRight)
+
+row1.addWidget(title, alignment=Qt.AlignCenter)
+row2.addWidget(minusButton, alignment=Qt.AlignLeft)
+row2.addWidget(plusButton, alignment=Qt.AlignRight)
 
 master_layout.addLayout(row1)
 master_layout.addLayout(row2)
-master_layout.addLayout(row3)
+
 
 main_win.setLayout(master_layout)
 
