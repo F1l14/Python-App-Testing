0d0bc32c6a6cec74b50bc4066526356c4f9c3e03
F1l14 <filipmn42@gmail.com>
2025-02-11 19:05:41 +0200

    Design Update

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel,QListWidget, QComboBox, QVBoxLayout, QHBoxLayout
-
+from PyQt5.QtCore import Qt
 class Editor(QWidget):
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle("Image Editor")
-        self.resize(500, 500)
+        self.resize(900, 900)
 
-
+         #* Properties -------------------------------
         self.folderB = QPushButton("Select Folder")
         self.fileList = QListWidget()
         self.comboB = QComboBox()
@@ -18,10 +18,20 @@ class Editor(QWidget):
         self.bwB = QPushButton("B/W")
         self.colorB = QPushButton("Color")
         self.contrastB = QPushButton("Contrast")
-        col1_widgets = [self.folderB, self.fileList, self.comboB, self.leftB, self.rightB, self.mirrorB, self.sharpB, self.bwB, self.colorB, self.contrastB]
+        self.blurB = QPushButton("Blur")
+        col1_widgets = [self.folderB, self.fileList, self.comboB, self.leftB, self.rightB, self.mirrorB, self.sharpB, self.bwB, self.colorB, self.contrastB, self.blurB]
         
+         #* Combo -------------------------------
+        combo_items = ["Original", "Left", "Right", "Mirror", "Sharpen", "B/W", "Color", "Contrast", "Blur"]
+        
+        for item in combo_items:
+            self.comboB.addItem(item)
+
+
+
         self.pictureView = QLabel("Picture View")
         
+        #* Design -------------------------------
         self.master_layout = QHBoxLayout()
         self.col1 = QVBoxLayout()
         self.col2 = QVBoxLayout()
@@ -31,8 +41,9 @@ class Editor(QWidget):
         
         self.col2.addWidget(self.pictureView)
 
-        self.master_layout.addLayout(self.col1)
-        self.master_layout.addLayout(self.col2)
+        #Adjusting "Stretch", 20%
+        self.master_layout.addLayout(self.col1, 20)
+        self.master_layout.addLayout(self.col2, 80)
         self.setLayout(self.master_layout)
 
 
